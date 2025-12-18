e0ea891aab12308ffc8fc4b19fa806f79ba7ab41
Brayan Adeolá <brayanadeola@gmail.com>
2025-12-17 22:13:02 -0300

    feat: tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,34 @@
+import { icons } from '@/constants/icons';
+import { images } from '@/constants/images';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ImageBackground, Image } from 'react-native';
+
+const TabIcon = () => {
+    return (
+        <ImageBackground
+            source={images.highlight}
+            className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
+            <Image
+                source={icons.home} tintColor="#151312" className="size-5"
+            />
+            <Text className='text-secondary text-base font-semibold ml-2'>Home</Text>
+        </ImageBackground>
+    )
+}
 
 const _Layout = () => {
     return (
         <Tabs>
             <Tabs.Screen
                 name="index"
-                options={{ headerShown: false, title: 'Home' }}
+                options={{ 
+                    headerShown: false, 
+                    title: 'Home',
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon />
+                    )
+                }}
             />
             <Tabs.Screen
                 name="search"
