9486ddaf44f03133af22b462cc868cba23d9a3ad
Brayan Adeolá <brayanadeola@gmail.com>
2026-01-12 23:17:24 -0300

    feat: programming search tab

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,72 @@
+import MovieCard from '@/components/MovieCard';
+import { icons } from '@/constants/icons';
+import { images } from '@/constants/images';
+import { fetchMovies } from '@/services/api';
+import useFetch from '@/services/useFetch';
+import { useRouter } from 'expo-router';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image, FlatList, ActivityIndicator, Text } from 'react-native';
+import { SearchBar } from 'react-native-screens';
 
 const Search = () => {
+    const router = useRouter();
+
+    const { 
+        data: movies, 
+        loading, 
+        error 
+      } = useFetch(() => fetchMovies({
+        query: '',
+    }));
+
     return (
-        <View>
-            
+        <View className='flex-1 bg-primary'>
+            <Image source={images.bg} className='flex-1 absolute w-full z-0' resizeMode='cover'/>
+
+            <FlatList 
+                data={movies} 
+                renderItem={({item}) => <MovieCard {... item}/>}
+                keyExtractor={(item) => item.id.toString()}
+                className='px-5'
+                numColumns={3}
+                columnWrapperStyle={{ 
+                    justifyContent: 'space-between', 
+                    gap: 16,
+                    marginBottom: 16
+                }}
+                contentContainerStyle={{ paddingBottom: 100 }}
+                ListHeaderComponent={
+                    <>
+                        <View className='w-full flex-row justify-center mt-20 items-center'>
+                            <Image source={icons.logo} className='w-12 h-10' resizeMode='contain'/>
+                        </View>
+                        <View className='my-5'>
+                            <SearchBar placeholder='Search for a movie...'/>
+                        </View>
+
+                        {loading && (
+                            <ActivityIndicator 
+                              size="large" 
+                              color="#0000ff" 
+                              className="ml-3" 
+                            />
+                        )}
+
+                        {error && (
+                            <Text className="text-red-500 px-5 my-3">
+                                Error: {error.message}
+                            </Text>
+                        )}
+
+                        {!loading && !error && 'SEARCH TERM'.trim() && movies?.lenght > 0 && (
+                            <Text className='text-xl text-white font-bold'>
+                                Search Results for {``}
+                                <Text className="text-accent">SEARCH TERM</Text>
+                            </Text>
+                        )}
+                    </>
+                }
+            />
         </View>
     );
 }
